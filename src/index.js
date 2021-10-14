2c4911f24030f38d8d9788799ce594b18dfbc3e7
Roman <rekun.olya@gmail.com>
2021-10-14 16:00:32 +0300

    hoc weather

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import WeatherComponent from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WeatherComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
